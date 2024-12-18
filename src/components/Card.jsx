1cb333cc79bbed6327fb907d59807a20e721e5a9
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2024-12-18 18:31:17 +0100

    ADD: addedd language flags

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import flagMaps from "./Flags"
+
 export default function Card ( {item} ) {
 
     const { title, original_title , name, original_name,  vote_average , original_language, poster_path } = item
@@ -6,7 +8,11 @@ export default function Card ( {item} ) {
         <div>
             <h3>{title}</h3>
             <p>{original_title}</p>
-            <p>{original_language}</p>
+            {
+                flagMaps[original_language] ? 
+                <img src={flagMaps[original_language]} height={20} alt="Immagine della bandiera della lingua originale" /> :
+                <p>{original_language}</p>   
+            }
             <p>{vote_average}</p>
         </div>
     )
---
+++ src/components/Flags.js
@@ -0,0 +1,15 @@
+import itFlag from '../assets/flags/it.webp'
+import engFlag from '../assets/flags/eng.png'
+import deFlag from '../assets/flags/de.png'
+import frFlag from '../assets/flags/fr.png'
+
+
+const flagMaps = {
+
+    'it' : itFlag,
+    'en' :engFlag,
+    'de' :deFlag,
+    'fr' :frFlag,
+}
+
+export default flagMaps
