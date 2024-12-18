a7573d1204d2ae92cc13e56c4b8b2bb82fa817f6
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2024-12-18 18:41:29 +0100

    ADD: poster's imgs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,13 @@ export default function Card ( {item} ) {
 
     const { title, original_title , name, original_name,  vote_average , original_language, poster_path } = item
 
+    const imgSrc = poster_path ? `https://image.tmdb.org/t/p/w300${poster_path}` : `https://i1.wp.com/potafiori.com/wp-content/uploads/2020/04/placeholder.png?ssl=1`
+
     return (
         <div>
+            <figure>
+                <img src={imgSrc} alt="" />
+            </figure>
             <h3>{title}</h3>
             <p>{original_title}</p>
             {
